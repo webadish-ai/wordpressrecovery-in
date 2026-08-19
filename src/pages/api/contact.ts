fb12f7c64ce88e1e57d49072af802d84d88f9057
diliplp <dilipparmar@gmail.com>
2026-08-19 09:21:07 +0530

    api: update contact email sender to help@hackedwebsiterecovery.in

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -24,7 +24,7 @@ export const POST: APIRoute = async ({ request }) => {
   const resend = new Resend(apiKey);
 
   const { error } = await resend.emails.send({
-    from: 'WordPressRecovery.in <help@webadish.com>',
+    from: 'HackedWebsiteRecovery.in <help@hackedwebsiterecovery.in>',
     to: ['snehal@webadish.com'],
     bcc: ['dilipparmar@gmail.com'],
     reply_to: body.email,
